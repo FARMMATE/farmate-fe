f90d6e1d3b7077f3fc251b3ab231bfe9721c3ee7
jminkkk <minl741@naver.com>
2023-08-28 01:41:05 +0900

    chore: Back-end 초기 세팅

---
+++ Back-end/src/main/java/com/example/ifwinfirstplace/IfWinFirstPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ifwinfirstplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfWinFirstPlaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IfWinFirstPlaceApplication.class, args);
+    }
+
+}

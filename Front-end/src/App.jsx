facf4849ca16e53d47979c8b574f90f1432cddd3
baegyeong <beth0929@naver.com>
2023-08-30 03:30:57 +0900

    feat: business route 설정

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -3,14 +3,17 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
 import DetailProduct from "./pages/DetailProduct";
+import Business from "./pages/Business";
 import routes from "./routes.js";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path={routes.login} element={<LoginPage />} />
         <Route path={routes.detailProduct} element={<DetailProduct />} />
+        <Route path={routes.business} element={<Business />} />
       </Routes>
     </>
   );
--- Front-end/src/routes.js
+++ Front-end/src/routes.js
@@ -2,6 +2,7 @@ const routes = {
   home: "/",
   login: "/login",
   detailProduct: "/detail-product",
+  business: "/business",
 };
 
 export default routes;

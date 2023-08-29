81540685f448b49977c981b268653de862a3af7a
K_Gs:D <rootachieve6053@gmail.com>
2023-08-30 02:27:59 +0900

    feat: 웹과 연결

--- Android/app/src/main/java/com/ifwinfirstplace/farmate/view/login/LoginFragment.kt
+++ Android/app/src/main/java/com/ifwinfirstplace/farmate/view/login/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.ifwinfirstplace.farmate.Const
 import com.ifwinfirstplace.farmate.R
 import com.ifwinfirstplace.farmate.databinding.FragmentLoginBinding
 
@@ -24,6 +25,10 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.webView.apply {
+            loadUrl(Const.ip+":"+Const.port)
+            settings.javaScriptEnabled = true
+        }
     }
 
     override fun onDestroyView() {

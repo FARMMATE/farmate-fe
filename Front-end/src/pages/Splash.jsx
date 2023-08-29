7199287ae3bb6c5e0c39c83b5ba7e51c2280052a
baegyeong <beth0929@naver.com>
2023-08-30 03:42:00 +0900

    feat: Splash 컴포넌트 구현

---
+++ Front-end/src/pages/Splash.jsx
@@ -0,0 +1,22 @@
+import OrangeBtn from "../components/OrangeBtn";
+import { useNavigate } from "react-router-dom";
+import routes from "../routes";
+
+const Splash = () => {
+  const navigate = useNavigate();
+
+  const handleOnClick = () => {
+    navigate(routes.login);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center w-[100vw] h-[100vh]">
+      <img className="w-[16rem]" src="/farmate-logo.png" alt="logo" />
+      <div className="drop-shadow-2xl fixed bottom-12">
+        <OrangeBtn onClick={handleOnClick}>시작하기</OrangeBtn>
+      </div>
+    </div>
+  );
+};
+
+export default Splash;

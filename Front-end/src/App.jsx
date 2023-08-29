8fe078636a3884eaa1e5c0dcecc3cdb9fef5baef
baegyeong <beth0929@naver.com>
2023-08-30 02:35:53 +0900

    fix: flex 속성 적용

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import DetailProduct from "./pages/DetailProduct";
+import routes from "./routes.js";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path={routes.detailProduct} element={<DetailProduct />} />
       </Routes>
     </>
   );

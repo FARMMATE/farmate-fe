57a0a6b40a5ee77fb63867d60533c2baad67759a
baegyeong <beth0929@naver.com>
2023-08-30 03:29:32 +0900

    fix: 버튼 height 변경

--- Front-end/src/components/OrangeBtn.jsx
+++ Front-end/src/components/OrangeBtn.jsx
@@ -1,9 +1,6 @@
 const OrangeBtn = ({ onClick, children }) => {
   return (
-    <button
-      className="text-pretendard-l w-[22rem] h-[3rem] rounded-xl bg-orange text-white font-bold"
-      onClick={onClick}
-    >
+    <button className="text-pretendard-l w-[22rem] h-14 rounded-xl bg-orange text-white font-bold" onClick={onClick}>
       {children}
     </button>
   );

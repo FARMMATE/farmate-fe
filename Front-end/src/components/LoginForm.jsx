72183a511346e19f23448337b3433e9f45f52810
baegyeong <beth0929@naver.com>
2023-08-30 02:40:12 +0900

    fix: 로그인 버튼 클릭시 navigate 지정

--- Front-end/src/components/LoginForm.jsx
+++ Front-end/src/components/LoginForm.jsx
@@ -2,8 +2,17 @@ import Input from "./Input";
 import OrangeBtn from "./OrangeBtn";
 import GrayBtn from "./GrayBtn";
 import SubLabel from "./SubLabel";
+import { email, passwd } from "../utils/userInfo";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import routes from "../routes";
 
 const LoginForm = () => {
+  const navigate = useNavigate();
+
+  const handleOnClick = () => {
+    navigate(routes.detailProduct);
+  };
   return (
     <>
       <div className="text-xl font-semibold text-left mt-16 mb-20 font-pretendard-r">로그인</div>
@@ -13,7 +22,7 @@ const LoginForm = () => {
       <Input id="passwd" name="passwd" type="password" placeholder="******">
         비밀번호
       </Input>
-      <OrangeBtn>로그인</OrangeBtn>
+      <OrangeBtn onClick={handleOnClick}>로그인</OrangeBtn>
       <div className="mt-8 flex flex-row justify-center mb-10">
         <SubLabel>회원 가입</SubLabel>
         <div className="border-l-2 mr-5" />

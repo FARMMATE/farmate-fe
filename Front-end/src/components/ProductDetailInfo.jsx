5a87413eaa698a9f78428754194a5fe565ffde56
baegyeong <beth0929@naver.com>
2023-08-30 02:39:13 +0900

    feat: ProductDetailInfo 컴포넌트 구현

---
+++ Front-end/src/components/ProductDetailInfo.jsx
@@ -0,0 +1,11 @@
+const ProductDetailInfo = ({ type, children, src }) => {
+  return (
+    <div className="mt-10 p-7 flex flex-col items-start font-pretendard-l">
+      <div className="font-pretendard-r text-xl font-bold mb-3">{type}</div>
+      <img src={src} alt="farm-info" />
+      <div className="mt-5 text-start text-xs">{children}</div>
+    </div>
+  );
+};
+
+export default ProductDetailInfo;

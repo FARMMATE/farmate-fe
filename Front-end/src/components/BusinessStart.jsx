0eeb3de9a69dedbbd9a73e824c45f9f95ce2da01
baegyeong <beth0929@naver.com>
2023-08-30 04:43:58 +0900

    feat: BusinessStart 컴포넌트 구현

---
+++ Front-end/src/components/BusinessStart.jsx
@@ -0,0 +1,14 @@
+const BusinessStart = () => {
+  return (
+    <div className="font-pretendard-l flex flex-col justify-start text-left p-6 mt-[25rem]">
+      <div className="font-pretendard-r text-3xl font-bold mb-10">나의 시안</div>
+      <div>
+        선택하신 상품은 <span className="text-orange font-bold">For Business</span>입니다!
+        <br /> 컨택 시 과정은 아래와 같습니다.
+      </div>
+      <div>시안 등록 -&gt; 채택 확정 -&gt; 담당자와의 합의 -&gt; 계약 확정</div>
+    </div>
+  );
+};
+
+export default BusinessStart;

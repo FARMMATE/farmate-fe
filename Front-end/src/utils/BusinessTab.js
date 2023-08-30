25eac34cbef312da2775bd733f986c5da12186fc
baegyeong <beth0929@naver.com>
2023-08-30 11:17:03 +0900

    feat: Business tab 관련 data

---
+++ Front-end/src/utils/BusinessTab.js
@@ -0,0 +1,4 @@
+export const BusinessTab = ["업체 소개", "시안 등록", "부가 설명"];
+export const intro = ["업체를 소개해주세요.", "경력이나 이력에 대해 말씀해주셔도 좋아요 :)"];
+export const register = ["시안을 등록 해주세요.", "시안에 대한 설명을 작성해주세요!"];
+export const detail = ["추가 정보를 작성해주세요.", "당부의 말을 작성하셔도 좋아요! :)"];

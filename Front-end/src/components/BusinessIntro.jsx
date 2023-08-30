41bd173ab8956ca728249698cd00e8f3c767418f
baegyeong <beth0929@naver.com>
2023-08-30 11:38:45 +0900

    fix: BusinessIntro 컴포넌트 재사용을 위한 prop 지정

--- Front-end/src/components/BusinessIntro.jsx
+++ Front-end/src/components/BusinessIntro.jsx
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 import { intro } from "../utils/BusinessTab";
 
-const BusinessIntro = () => {
+const BusinessIntro = ({ intro, placeholder }) => {
   const textarea = useRef();
 
   const handleResizeHeight = () => {
@@ -11,14 +11,14 @@ const BusinessIntro = () => {
 
   return (
     <div className="font-pretendard-l bg-gradient-to-b from-orange rounded-xl p-6 h-[45rem] w-[24rem] text-left">
-      <div className="font-bold text-2xl mb-5">{intro[0]}</div>
+      <div className="font-bold text-2xl mb-5">{intro}</div>
       <textarea
         type="text"
         ref={textarea}
         onChange={handleResizeHeight}
         rows={1}
         className="bg-white outline-none text-gray-900 text-sm rounded-lg block w-full p-2.5 max-h-[30rem] min-h-[6rem]"
-        placeholder={intro[1]}
+        placeholder={placeholder}
       />
     </div>
   );

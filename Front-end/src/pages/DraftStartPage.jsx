2661360843c809e95557e537b1740a36525ca4a6
baegyeong <beth0929@naver.com>
2023-08-30 23:52:22 +0900

    fix: BusinessStart 컴포넌트 명칭 변경 반영

--- Front-end/src/pages/DraftStartPage.jsx
+++ Front-end/src/pages/DraftStartPage.jsx
@@ -1,4 +1,4 @@
-import BusinessStart from "../components/BusinessStart";
+import DraftStart from "../components/DraftStart";
 import OrangeBtn from "../components/OrangeBtn";
 import { useNavigate, useLocation } from "react-router-dom";
 import routes from "../routes";
@@ -10,7 +10,7 @@ const DraftStartPage = () => {
 
   return (
     <div className="flex items-center flex-col w-[100vw] h-[100vh]">
-      <BusinessStart id={type} />
+      <DraftStart id={type} />
       <div className="drop-shadow-2xl fixed bottom-12">
         <OrangeBtn onClick={() => navigate(routes.draftRegister)}>다음으로</OrangeBtn>
       </div>

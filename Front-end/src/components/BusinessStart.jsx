442cc6659dbd215ed43ba0bd1eedaf97a6612e16
baegyeong <beth0929@naver.com>
2023-08-30 17:26:16 +0900

    fix: css 크기 변경

--- Front-end/src/components/BusinessStart.jsx
+++ Front-end/src/components/BusinessStart.jsx
@@ -1,15 +1,17 @@
 const BusinessStart = () => {
   return (
-    <div className="font-pretendard-l flex flex-col justify-start text-left p-6 mt-[10rem]">
-      <div className="animate-fadein">
+    <div className="font-pretendard-l flex flex-col justify-start items-center text-left p-6 mt-[8rem]">
+      <div className="animate-fadein w-[70%]">
         <img src="/export.png" alt="export" />
       </div>
-      <div className="font-pretendard-r text-3xl font-bold mt-[4rem] mb-10">전문가를 원해요!</div>
-      <div>
-        선택하신 상품은 <span className="text-orange font-bold">For Business</span>입니다!
-        <br /> 컨택 시 과정은 아래와 같습니다.
+      <div className="flex flex-col justify-start">
+        <div className="font-pretendard-r text-3xl font-bold mt-[4rem] mb-10">전문가를 원해요!</div>
+        <div>
+          선택하신 상품은 <span className="text-orange font-bold">For Business</span>입니다!
+          <br /> 컨택 시 과정은 아래와 같습니다.
+        </div>
+        <div className="mt-5">시안 등록 -&gt; 채택 확정 -&gt; 담당자와의 합의 -&gt; 계약 확정</div>
       </div>
-      <div className="mt-5">시안 등록 -&gt; 채택 확정 -&gt; 담당자와의 합의 -&gt; 계약 확정</div>
     </div>
   );
 };

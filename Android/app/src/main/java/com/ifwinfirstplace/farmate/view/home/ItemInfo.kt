7810976b8e40919f1466b56c891ca4cde65317e3
K_Gs:D <rootachieve6053@gmail.com>
2023-08-30 17:07:15 +0900

    feat: home 하단 상품

---
+++ Android/app/src/main/java/com/ifwinfirstplace/farmate/view/home/ItemInfo.kt
@@ -0,0 +1,12 @@
+package com.ifwinfirstplace.farmate.view.home
+
+
+data class ItemInfo(
+    val location : String,
+    val title : String,
+    val target : String,
+    val logoImgId : Int,
+    val mainImgId : Int,
+    val isMatching : Boolean
+
+)

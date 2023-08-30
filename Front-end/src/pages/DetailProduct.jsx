57cd58bd97689e1490f30517ce92ddddc18e7472
baegyeong <beth0929@naver.com>
2023-08-30 19:39:42 +0900

    feat: 스크랩 현황 토스트 추가

--- Front-end/src/pages/DetailProduct.jsx
+++ Front-end/src/pages/DetailProduct.jsx
@@ -5,9 +5,25 @@ import { farm } from "../utils/farm";
 import { product } from "../utils/product";
 import { useNavigate } from "react-router-dom";
 import routes from "../routes";
+import { useEffect, useState } from "react";
 
 const DetailProduct = () => {
   const navigate = useNavigate();
+  const [toast, setToast] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 70) {
+        setToast(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <>
@@ -19,8 +35,16 @@ const DetailProduct = () => {
         <ProductDetailInfo type={product[0]} src="/strawberry2.png">
           {product[1]}
         </ProductDetailInfo>
-        <div className="h-20" />
+        {toast && (
+          <div className="flex justify-center align-center">
+            <div className="font-pretendard-l fixed z-20 rounded-2xl bg-dark-black w-[15rem] h-8 p-1 text-white bottom-[7rem] animate-godown">
+              현재 10명이 스크랩했어요!
+            </div>
+          </div>
+        )}
+        <div className="h-[10rem]" />
       </div>
+
       <div className="fixed z-20 bottom-10 drop-shadow-xl bg-transparent" accept="pdf image/*">
         <OrangeBtn onClick={() => navigate(routes.business)}>컨택하기</OrangeBtn>
       </div>

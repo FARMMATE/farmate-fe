1bf1e33ac0de7e4d89051089f20ceb9e401d9459
baegyeong <beth0929@naver.com>
2023-08-31 02:14:06 +0900

    fix: 로그인 버튼 클릭시 로고 fadeout 추가

--- Front-end/src/pages/Splash.jsx
+++ Front-end/src/pages/Splash.jsx
@@ -1,17 +1,26 @@
 import OrangeBtn from "../components/OrangeBtn";
 import { useNavigate } from "react-router-dom";
 import routes from "../routes";
+import { useState } from "react";
 
 const Splash = () => {
   const navigate = useNavigate();
+  const [goLogin, setGoLogin] = useState(false);
 
   const handleOnClick = () => {
-    navigate(routes.login);
+    setGoLogin(true);
+    setTimeout(() => {
+      navigate(routes.login);
+    }, 400);
   };
 
   return (
     <div className="flex flex-col items-center justify-center w-[100vw] h-[100vh]">
-      <img className="w-[16rem] mb-24 animate-godown" src="/farmate-logo.png" alt="logo" />
+      <img
+        className={goLogin ? "w-[16rem] mb-24 animate-logofadeout" : "w-[16rem] mb-24 animate-godown"}
+        src="/farmate-logo.png"
+        alt="logo"
+      />
       <div className="drop-shadow-2xl fixed bottom-12 animate-goup">
         <OrangeBtn onClick={handleOnClick}>시작하기</OrangeBtn>
       </div>

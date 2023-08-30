38404f8bd0fc96694121451fcfe444cf3755aa15
baegyeong <beth0929@naver.com>
2023-08-30 21:52:04 +0900

    fix: 사용하지 않는 파일 삭제

--- Front-end/src/components/GrayBtn.jsx
+++
@@ -1,9 +0,0 @@
-const GrayBtn = ({ onClick, children }) => {
-  return (
-    <button className="w-[22rem] h-[3rem] rounded-xl text-black text-pretendard-l text-sm" onClick={onClick}>
-      {children}
-    </button>
-  );
-};
-
-export default GrayBtn;

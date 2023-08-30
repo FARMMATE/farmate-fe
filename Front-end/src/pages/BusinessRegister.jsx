86b8e5fc7b4f1ad5785af49688fcf1f16c0d368b
baegyeong <beth0929@naver.com>
2023-08-30 15:13:53 +0900

    feat: 탭 이동할 때마다 아래 컴포넌트 수정

--- Front-end/src/pages/BusinessRegister.jsx
+++ Front-end/src/pages/BusinessRegister.jsx
@@ -8,9 +8,17 @@ import { useState, useRef, useEffect } from "react";
 import { intro } from "../utils/BusinessTab";
 
 const BusinessRegister = () => {
-  const [clickIntro, setClickIntro] = useState(false);
-  const [clickRegister, setClickRegister] = useState(false);
-  const [clickDetail, setClickDetail] = useState(false);
+  const [clickTab, setClickTab] = useState([true, false, false]);
+
+  const handleOnClick = (id) => {
+    if (id === "intro") {
+      setClickTab([true, false, false]);
+    } else if (id === "register") {
+      setClickTab([false, true, false]);
+    } else if (id === "detail") {
+      setClickTab([false, false, true]);
+    }
+  };
 
   const introFocus = useRef();
   useEffect(() => {
@@ -24,14 +32,16 @@ const BusinessRegister = () => {
       <div className="flex flex-col m-5">
         <div className="flex flex-col justify-start items-start text-left">
           <div className="flex mb-6">
-            <span>
-              <DraftTab ref={introFocus}>{BusinessTab[0]}</DraftTab>
-            </span>
-            <DraftTab>{BusinessTab[1]}</DraftTab>
-            <DraftTab>{BusinessTab[2]}</DraftTab>
+            <DraftTab ref={introFocus} onClick={() => handleOnClick("intro")}>
+              {BusinessTab[0]}
+            </DraftTab>
+            <DraftTab onClick={() => handleOnClick("register")}>{BusinessTab[1]}</DraftTab>
+            <DraftTab onClick={() => handleOnClick("detail")}>{BusinessTab[2]}</DraftTab>
           </div>
         </div>
-        <BusinessIntro intro={intro[0]} placeholder={intro[1]} />
+        {clickTab[0] ? <BusinessIntro intro={intro[0]} placeholder={intro[1]} /> : null}
+        {clickTab[1] ? <BusinessIntro intro={register[0]} placeholder={register[1]} /> : null}
+        {clickTab[2] ? <BusinessIntro intro={detail[0]} placeholder={detail[1]} /> : null}
         <div className="drop-shadow-2xl fixed bottom-12 ml-4">
           <OrangeBtn>정보 등록하기</OrangeBtn>
         </div>

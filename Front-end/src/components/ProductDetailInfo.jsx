47ffb58102915d72d74b1f600add2f953e756de4
baegyeong <beth0929@naver.com>
2023-08-30 23:51:41 +0900

    fix: img 크기 변경

--- Front-end/src/components/ProductDetailInfo.jsx
+++ Front-end/src/components/ProductDetailInfo.jsx
@@ -1,8 +1,10 @@
 const ProductDetailInfo = ({ type, children, src }) => {
   return (
-    <div className="mt-10 p-7 flex flex-col items-start font-pretendard-l">
-      <div className="font-pretendard-r text-xl font-bold mb-3">{type}</div>
-      <img src={src} alt="farm-info" />
+    <div className="mt-10 p-7 flex flex-col font-pretendard-l">
+      <div className="font-pretendard-r text-xl font-bold mb-3 items-start text-left">{type}</div>
+      <div className="flex justify-center items-center">
+        <img className="w-[22rem]" src={src} alt="farm-info" />
+      </div>
       <div className="mt-5 text-start text-sm leading-6">{children}</div>
     </div>
   );

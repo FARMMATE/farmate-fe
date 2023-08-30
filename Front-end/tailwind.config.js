fb806f02a21c6c565ac70203a815607a7e6af80b
baegyeong <beth0929@naver.com>
2023-08-30 17:06:46 +0900

    feat: fadin, to-up 애니메이션 지정

--- Front-end/tailwind.config.js
+++ Front-end/tailwind.config.js
@@ -19,13 +19,29 @@ export default {
       },
       keyframes: {
         fadein: {
-          from: {
+          "0%": {
             opacity: 0,
+            transform: "translate3d(-100%, 0, 0)",
           },
           to: {
             opacity: 1,
+            transform: "translateZ(0)",
           },
         },
+        toup: {
+          "0%": {
+            opacity: 0,
+            transform: "translate3d(0, -100%, 0)",
+          },
+          to: {
+            opacity: 1,
+            transform: "translateZ(0)",
+          },
+        },
+      },
+      animation: {
+        fadein: "fadein 0.5s ease-out",
+        toup: "toup 0.5s ease-out",
       },
     },
     fontFamily: {

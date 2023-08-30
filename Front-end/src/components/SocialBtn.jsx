23e51f9987728ab1812c5d7ef5cc1e511aee8c35
baegyeong <beth0929@naver.com>
2023-08-30 23:08:33 +0900

    fix: 소셜 로그인 컴포넌트 수정

--- Front-end/src/components/SocialBtn.jsx
+++ Front-end/src/components/SocialBtn.jsx
@@ -1,7 +1,10 @@
-const SocialBtn = ({ onClick, children }) => {
+const SocialBtn = ({ onClick, children, src }) => {
   return (
-    <button className="w-[22rem] h-[3rem] rounded-xl text-black text-pretendard-l text-sm" onClick={onClick}>
-      {children}
+    <button className="w-[22rem] h-[3rem] rounded-xl text-black text-[1rem]" onClick={onClick}>
+      <div className="flex items-center justify-center mr-4">
+        <img src={src} className="w-[3rem]" />
+        <span className="font-pretendard-r">{children}</span>
+      </div>
     </button>
   );
 };

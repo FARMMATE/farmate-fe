3b9822f2f129942ebb7f26c970a595dd17632d7c
baegyeong <beth0929@naver.com>
2023-08-30 16:39:57 +0900

    fix: export 이미지 추가

--- Front-end/src/components/BusinessStart.jsx
+++ Front-end/src/components/BusinessStart.jsx
@@ -1,12 +1,13 @@
 const BusinessStart = () => {
   return (
-    <div className="font-pretendard-l flex flex-col justify-start text-left p-6 mt-[25rem]">
-      <div className="font-pretendard-r text-3xl font-bold mb-10">나의 시안</div>
+    <div className="font-pretendard-l flex flex-col justify-start text-left p-6 mt-[10rem]">
+      <img src="/export.png" alt="export" />
+      <div className="font-pretendard-r text-3xl font-bold mt-[4rem] mb-10">전문가를 원해요!</div>
       <div>
         선택하신 상품은 <span className="text-orange font-bold">For Business</span>입니다!
         <br /> 컨택 시 과정은 아래와 같습니다.
       </div>
-      <div>시안 등록 -&gt; 채택 확정 -&gt; 담당자와의 합의 -&gt; 계약 확정</div>
+      <div className="mt-5">시안 등록 -&gt; 채택 확정 -&gt; 담당자와의 합의 -&gt; 계약 확정</div>
     </div>
   );
 };

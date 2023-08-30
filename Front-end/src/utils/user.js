9b8675db085e186a7b846b75d562ee1667f162f3
baegyeong <beth0929@naver.com>
2023-08-30 23:28:42 +0900

    chore: 목데이터 설정

--- Front-end/src/utils/user.js
+++ Front-end/src/utils/user.js
@@ -1,1 +1,2 @@
-export const user = ["For Business", "For Supporter"];
+export const businessUser = ["전문가", "For Business", "시안 등록 -> 채택 확정 -> 담당자 협의 -> 계약 확정"];
+export const supportUser = ["서포터즈", "For Supporter", "디자인 등록 -> 채택 확정 -> 담당자 협의 -> 상표 등록"];

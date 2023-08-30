54ac92e785f3461acc3ae0c4d8e9271783ed4255
baegyeong <beth0929@naver.com>
2023-08-30 16:27:03 +0900

    fix: font 사이즈 변경

---
+++ Front-end/src/components/Business.jsx
@@ -0,0 +1,27 @@
+import { useRef } from "react";
+import { intro } from "../utils/BusinessTab";
+
+const Business = ({ intro, placeholder }) => {
+  const textarea = useRef();
+
+  const handleResizeHeight = () => {
+    textarea.current.style.height = "auto";
+    textarea.current.style.height = textarea.current.scrollHeight + "px";
+  };
+
+  return (
+    <div className="font-pretendard-l bg-gradient-to-b from-orange from-60% rounded-xl p-6 h-[45rem] w-[24rem] text-left">
+      <div className="font-bold text-2xl mb-5">{intro}</div>
+      <textarea
+        type="text"
+        ref={textarea}
+        onChange={handleResizeHeight}
+        rows={1}
+        className="bg-white outline-none text-gray-900 rounded-lg block w-full p-2.5 max-h-[30rem] min-h-[6rem]"
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
+
+export default Business;

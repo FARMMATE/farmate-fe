7c2728f98ee0ee6118465533a2859a3599c884be
baegyeong <beth0929@naver.com>
2023-08-30 20:18:12 +0900

    fix: 글자 크기 및 행간 높이 변경

--- Front-end/src/components/ProductDetailInfo.jsx
+++ Front-end/src/components/ProductDetailInfo.jsx
@@ -3,7 +3,7 @@ const ProductDetailInfo = ({ type, children, src }) => {
     <div className="mt-10 p-7 flex flex-col items-start font-pretendard-l">
       <div className="font-pretendard-r text-xl font-bold mb-3">{type}</div>
       <img src={src} alt="farm-info" />
-      <div className="mt-5 text-start text-xs">{children}</div>
+      <div className="mt-5 text-start text-sm leading-6">{children}</div>
     </div>
   );
 };

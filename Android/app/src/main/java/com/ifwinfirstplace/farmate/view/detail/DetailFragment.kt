217221a07e8afb4c23c47d2e2ed6f02913c72e55
K_Gs:D <rootachieve6053@gmail.com>
2023-08-30 14:41:21 +0900

    feat: 상세 프래그먼트 생성

--- Android/app/src/main/java/com/ifwinfirstplace/farmate/view/detail/DetailFragment.kt
+++ Android/app/src/main/java/com/ifwinfirstplace/farmate/view/detail/DetailFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.ifwinfirstplace.farmate.Const
+import com.ifwinfirstplace.farmate.Page
+import com.ifwinfirstplace.farmate.PageChangeListener
 import com.ifwinfirstplace.farmate.R
 import com.ifwinfirstplace.farmate.databinding.FragmentDetailBinding
 import com.ifwinfirstplace.farmate.databinding.FragmentHomeBinding
+import com.ifwinfirstplace.farmate.view.login.LoginInterface
 
 class DetailFragment : Fragment() {
 
@@ -23,6 +28,17 @@ class DetailFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if(activity is PageChangeListener){
+            (activity as PageChangeListener).changePage(Page.DETAIL)
+        }
+        binding.webView.apply {
+            loadUrl(Const.ip+":"+ Const.port+"/detail-product")
+            settings.javaScriptEnabled = true
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null

21cb74ecb4297fa6cdb8f8a8acc87a1cee40aed2
baegyeong <beth0929@naver.com>
2023-08-30 11:37:47 +0900

    fix: forwardRef 지정

--- Front-end/src/components/DraftTab.jsx
+++ Front-end/src/components/DraftTab.jsx
@@ -1,15 +1,18 @@
-const DraftTab = ({ children, onClick }) => {
+import { forwardRef } from "react";
+
+const DraftTab = forwardRef(({ children, onClick }, ref) => {
   return (
-    <>
-      <button
-        type="click"
-        onClick={onClick}
-        className="font-pretendard-l focus:after:content-[''] focus:after:block focus:after:w-[4.3rem] focus:after:border-b-2 focus:after:border-b-black focus:font-pretendard-r text-lg focus:font-bold mr-7"
-      >
-        {children}
-      </button>
-    </>
+    <button
+      type="click"
+      onClick={onClick}
+      ref={ref}
+      className="font-pretendard-l focus:after:content-[''] focus:after:block focus:after:w-[4.3rem] focus:after:border-b-2 focus:after:border-b-black focus:font-pretendard-r text-lg focus:font-bold mr-7"
+    >
+      {children}
+    </button>
   );
-};
+});
+
+DraftTab.displayName = "DraftTab"; // displayName 추가
 
 export default DraftTab;
